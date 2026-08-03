a7606511c1737b8c13af32f4032d4ad61bb4a838
khalid-webdev <khaleduzzaman67@gmail.com>
2026-08-03 08:41:11 +0600

    adding login and refreshToken api

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require("../middleware/asyncHandler");
-const {registerSchema} = require("../validators/authValidator")
+const jwt = require("jsonwebtoken");
+const {registerSchema, loginSchema} = require("../validators/authValidator")
 const ApiError=require("../utils/ApiError");
 const User =require("../models/User");
 const {generateAccessToken,generateRefreshToken}=require("../utils/generateToken");
 const cookieOptions = require("../utils/cookieOption");
+const { jwtRefreshSecret } = require("../config/env");
 
-
+//^ register api
 exports.register = asyncHandler(async(req,res)=>{
   const {error,value}=registerSchema.validate(req.body);
   if(error){
@@ -29,4 +31,42 @@ exports.register = asyncHandler(async(req,res)=>{
     cookieOptions
   );
   res.status(201).json({success:true,accessToken,message:"Registration Successfull!",user:newUser.toJSON()});
+});
+
+//* login api
+exports.login = asyncHandler(async(req,res)=>{
+  const {error,value} = loginSchema.validate(req.body);
+  if(error){
+    throw new ApiError(400,error.details[0].message);
+  }
+  const {email,password} = value;
+  const user = await User.findOne({email}).select("+password");
+  if(!user){
+    throw new ApiError(401,"Invalid credentials!");
+  }
+  const isMatch = await user.comparePassword(password);
+  if(!isMatch){
+    throw new ApiError(401,"Invalid credentials!");
+  }
+  const accessToken = generateAccessToken(user._id,user.role);
+  const refreshToken = generateRefreshToken(user._id);
+  res.cookie("refreshToken",refreshToken,cookieOptions);
+  res.status(200).json({success:true,message:"Login successfull",accessToken,user:user.toJSON()});
+
+});
+
+//^refreshToken api it will be post request
+exports.refreshToken = asyncHandler(async(req,res)=>{
+  const token = req.cookies.refreshToken;
+  if(!token){
+    throw new ApiError(401,"Refresh token is missing!");
+  }
+  const decoded = jwt.verify(token,jwtRefreshSecret);
+  console.log(decoded);
+  const user = await User.findById(decoded.id);
+  if(!user){
+    throw new ApiError(401,"User not found!");
+  }
+  const accessToken = generateAccessToken(user._id,user.role);
+  res.status(200).json({success:true,accessToken});
 })
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { register } = require("../controller/authController");
+const { register, login, refreshToken } = require("../controller/authController");
 const router = express.Router();
 const {registerSchema, loginSchema} = require("../validators/authValidator");
 const validate = require("../middleware/validator");
 
 router.post("/register",validate(registerSchema),register);
-
+router.post("/login",validate(loginSchema),login);
+router.post("/refresh",refreshToken);
 
 module.exports=router;
